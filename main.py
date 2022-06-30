d1799313903f6924d378ffd30525984b4098931a
Albert Lewis <45643396+AlbertLewis@users.noreply.github.com>
2022-06-29 21:28:14 -0400

    added main, .gitignore, and authorization token

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import discord
+import os
+
+"""
+- reminder to pay rent @ing each person specifially when they don't pay
+- automatic printing
+- trash/recycle day 
+"""
+
 
 def main():
-    print("hello world")
+    client = discord.Client()
+    # load_dotenv()
+    client.run(os.getenv('TOKEN'))
+    print("testing")
 
 if __name__ == "__main__":
     main()
